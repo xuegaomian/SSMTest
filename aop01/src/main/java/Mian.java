4702fc02916fb3526ae11b27396808a64b613f5c
xuegaomian <13488322451@163.com>
2017-08-14 09:50:41 +0800

    dependce

--- aop01/src/main/java/Mian.java
+++ aop01/src/main/java/Mian.java
@@ -12,8 +12,11 @@ public class Mian {
         UserService userService= new UserServiceImpl();
         InvocationHandler invocationHandler=new UserInvocationHandl(userService);//让代理类去代理userService
         //得到代理对象userServiceProxy
+        //内部通过反射instance该类，然后在handler中执行invoke方法，进行操作
         UserService userServiceProxy= (UserService) Proxy.newProxyInstance(userService.getClass().getClassLoader(),
                 userService.getClass().getInterfaces(),invocationHandler);
         userServiceProxy.callName("laowang");
+
+        
     }
 }
