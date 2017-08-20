a25566d6e0c987ac6a28837bc276dca8d6686229
xuegaomian <13488322451@163.com>
2017-08-20 10:15:12 +0800

    mybatis

--- SSM03-mybatis/src/Main.java
+++ SSM03-mybatis/src/Main.java
@@ -5,6 +5,7 @@ import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
 import java.io.InputStream;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws Exception {
@@ -15,14 +16,16 @@ public class Main {
         SqlSession session=factory.openSession();
         StudentMapper mapper=session.getMapper(StudentMapper.class);
         /*Student student=new Student();
-        student.setSid(007);
+        student.setSid(78);
         student.setSname("laowang");
         student.setSage(78);
-        student.setSsex("男");*/
-        Student student=mapper.selectOneById(1);
+        student.setSsex("男");
+        int x=mapper.insertStudent(student);
+        System.out.println(x);*/
+        List<Student> studentList=mapper.seleceAllGoods();
         session.commit();
         session.close();
-        System.out.println(student);
+        System.out.println(studentList);
     }
 
 
--- SSM03-mybatis/src/com/xue/dao/StudentMapper.java
+++ SSM03-mybatis/src/com/xue/dao/StudentMapper.java
@@ -3,10 +3,12 @@ package com.xue.dao;
 
 import com.xue.po.Student;
 
+import java.util.List;
+
 public interface StudentMapper {
     int insertStudent(Student student)throws Exception;
 
     Student selectOneById(int id)throws Exception;
 
-    //List<Student> seleceAllGoods()throws Exception;
+    List<Student> seleceAllGoods()throws Exception;
 }
